6af45d9cba0b2cf4a2de6c45a8f6a75e6c4df157
hecctor <hector.pueyo@estudiantat.upc.edu>
2022-05-26 13:03:41 +0200

    sdjffsdoj

--- src/router/index.js
+++ src/router/index.js
@@ -15,9 +15,6 @@ import Submission from '@/components/Submission.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: '/',
-  fallback: true,
   routes: [
     {
       path: '/',
